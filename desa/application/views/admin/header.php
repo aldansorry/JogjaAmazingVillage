78d95afd025125322e1dbd61379efc627e18aedc
Bagus Pogar Herlambang <bagus.pogar@gmail.com>
2019-01-04 13:55:34 +0700

    Tampilan admin pengelola desa

---
+++ desa/application/views/admin/header.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Datatable - srtdash</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="<?php echo base_url('assets/') ?>assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/themify-icons.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/metisMenu.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/slicknav.min.css">
+    <!-- amcharts css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- Start datatable css -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.jqueryui.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/typography.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/default-css.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/styles.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="<?php echo base_url('assets/') ?>assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.html"><img src="<?php echo base_url('assets/') ?>assets/images/icon/logo.png" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                                <ul class="collapse">
+                                    <li><a href="index.html">Bagus</a></li>
+                                    <li><a href="index2.html">Ecommerce dashboard</a></li>
+                                    <li><a href="index3.html">SEO dashboard</a></li>
+                                </ul>
+                            </li>
+                            
+                            <li><a href="maps.html"><i class="ti-map-alt"></i> <span>maps</span></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
